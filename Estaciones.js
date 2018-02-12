cbe2a7ebb1f2720cc3255952078500c0a51c256c
Luis Alvares <luisalvrez214@gmail.com>
2018-02-12 08:31:55 -0600

    Subiendo EL repositorio a Github

---
+++ Estaciones.js
@@ -0,0 +1,22 @@
+var mes;
+mes=10;
+if(mes==12||mes==1||mes==2)
+  
+    alert("ES Invierno");
+    
+    else if(mes>=3 && mes<=5)
+      {
+        alert("Es Primavera");
+      }
+    else if(mes>=6&&mes<=8)
+    {
+      alert("Es verano");
+    }
+  else if(mes>=9||mes<=11)
+    {
+      alert("Es otoño |Nombre : Luis Arturo Alvarez Garcia| Carne: 18002357 |");
+	  
+    }
+	else 
+      
+	alert("NO exite Respuesta ");
